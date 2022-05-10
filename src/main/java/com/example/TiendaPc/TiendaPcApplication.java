77eb350b6e11cc01e617a034c9ec72d658cd0c1c
HIBERUS\abarrasa <abarrasa@hiberus.com>
2022-05-10 11:58:27 +0200

    V1

---
+++ src/main/java/com/example/TiendaPc/TiendaPcApplication.java
@@ -0,0 +1,13 @@
+package com.example.TiendaPc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPcApplication.class, args);
+	}
+
+}
